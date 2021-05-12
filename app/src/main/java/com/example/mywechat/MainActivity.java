5c4c90ce0b3885f7ecf40113fb9617d7a9e730cd
CRZbulabula <crzbulabula@gmail.com>
2021-05-12 14:29:20 +0800

    注册页预览

--- app/src/main/java/com/example/mywechat/MainActivity.java
+++ app/src/main/java/com/example/mywechat/MainActivity.java
@@ -12,16 +12,33 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setLoginView();
+    }
+
+    private void setLoginView() {
         setContentView(R.layout.activity_main);
         Button loginButton = findViewById(R.id.login_button);
-        Button registerButton = findViewById(R.id.register_button);
+        Button registerUserButton = findViewById(R.id.register_user_button);
 
         loginButton.setOnClickListener(v -> {
             //TODO: 增加登录验证
             startActivity(new Intent(MainActivity.this, UserActivity.class));
         });
+        registerUserButton.setOnClickListener(v -> {
+            setRegisterView();
+        });
+    }
+
+    private void setRegisterView() {
+        setContentView(R.layout.fragmeng_register);
+        Button loginUserButton = findViewById(R.id.login_user_button);
+        Button registerButton = findViewById(R.id.register_button);
+
+        loginUserButton.setOnClickListener(v -> {
+            setLoginView();
+        });
         registerButton.setOnClickListener(v -> {
-            //TODO: 进行注册验证
+            // TODO: 进行注册验证
         });
     }
 
